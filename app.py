ab3fc2d181086ff2c3ddfdfbd008948c61e12b40
tz7013 <tz7013@gmail.com>
2024-09-03 01:55:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from openai import OpenAI
 
 app = flask.Flask(__name__)
 
+auth_token="i3etM/dX+X8u1fxNRL7vkQAx2QHXzYWVmGqcw7WzHuqjgJne2EDcDHDAgE7R+EKvRFaXs0tp2L4SgVPo7VpD/G/vvvp78wZx5qrUsEK5F8oGLejqlo1eg4TdCzO8LRtMyGGPsKNZFTGzAgpKQS3FlAdB04t89/1O/w1cDnyilFU="
+YouruserID="U0e29664b46667d26b078ad2878cd4e94"
+
 # 初始化 OpenAI 客戶端
 client = OpenAI()
 
